e91e756c2a7d13a602d49c5e99ea20dc75392d90
C18433856 <C18433856@mytudublin.ie>
2022-11-13 17:38:27 +0000

    Added name filter

--- problem-1/Directory.js
+++ problem-1/Directory.js
@@ -1,5 +1,5 @@
 var background_var = 1;
-const tr_list = [];
+var sorting = "ascend";
 
 document.getElementById("insert").onclick = function(){
     const input_name = document.getElementById("Name");
@@ -19,20 +19,17 @@ document.getElementById("insert").onclick = function(){
 
         document.getElementById("contacts-list").appendChild(new_tr);
 
-        const new_th1 = document.createElement("td");
-        const new_th2 = document.createElement("td");
-        const new_th3 = document.createElement("td");
+        const new_td1 = document.createElement("td");
+        const new_td2 = document.createElement("td");
+        const new_td3 = document.createElement("td");
 
-        new_th1.innerHTML = input_name.value;
-        new_th2.innerHTML = input_mobile.value;
-        new_th3.innerHTML = input_email.value;
+        new_td1.innerHTML = input_name.value;
+        new_td2.innerHTML = input_mobile.value;
+        new_td3.innerHTML = input_email.value;
 
-        new_tr.appendChild(new_th1);
-        new_tr.appendChild(new_th2);
-        new_tr.appendChild(new_th3);
-
-        tr_list.push(new_tr);
-
+        new_tr.appendChild(new_td1);
+        new_tr.appendChild(new_td2);
+        new_tr.appendChild(new_td3);
 
     }
 
@@ -41,9 +38,30 @@ document.getElementById("insert").onclick = function(){
     input_email.value = "";
 }
 
+document.getElementById("name-header").onclick = function(){
+    let i, j;
+    let contactTable = document.getElementById("contacts-list");
+    let contacts = contactTable.rows;
+
+    if(sorting == "ascend"){
+        sorting = "descend";
+        for(i = 1; i < contacts.length - 1; i++)
+            for(j = 1; j < contacts.length - i; j++)
+                if(contacts[j].firstChild.innerHTML.toLowerCase() > contacts[j + 1].firstChild.innerHTML.toLowerCase())
+                    contactTable.insertBefore(contacts[j + 1], contacts[j]);
+    }
+    else if(sorting == "descend"){
+        sorting = "ascend";
+        for(i = 1; i < contacts.length - 1; i++)
+            for(j = 1; j < contacts.length - i; j++)
+                if(contacts[j].firstChild.innerHTML.toLowerCase() < contacts[j + 1].firstChild.innerHTML.toLowerCase())
+                    contactTable.insertBefore(contacts[j + 1], contacts[j]);
+    }
+}
+
 function ValidateInput(input_name, input_mobile, input_email){
     if (! /^[A-Za-z\s]*$/.test(input_name) || input_name.length > 20) return false;
     if (! /^[0-9]*$/.test(input_mobile) || input_mobile.length != 10) return false;
-    if (!input_email.includes("@") || input_email.length > 40) return false;
+    if (! input_email.includes("@") || input_email.length > 40) return false;
     return true;
 }
