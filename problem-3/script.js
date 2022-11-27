096f1d7b05a022b7dc71c410c49c57783eafed46
C18433856 <C18433856@mytudublin.ie>
2022-11-27 16:41:40 +0000

    Display repos and error checking

--- problem-3/script.js
+++ problem-3/script.js
@@ -1,8 +1,13 @@
 document.getElementById("search-button").onclick = function(){
     const to_search = document.getElementById("username-input").value;
+    document.getElementById("user-repos").innerHTML = "";
     fetch("https://api.github.com/users/" + to_search)
-    .then(response => response.json())
+    .then(response => {
+        if(response.ok) {return response.json()}
+        else {throw new Error("unable to retrieve user");}
+        })
     .then(user => fill_profile(user))
+    .catch(error => alert(error))
 }
 
 function fill_profile(user_data){
@@ -17,7 +22,20 @@ function fill_profile(user_data){
                                                                                     "Location: " + user_data.location);
     document.getElementById("gists").innerHTML = ((user_data.public_gists == null) ? "Number of Gists: Not Available" :
                                                                                      "Number of Gists: " + user_data.public_gists);
-
     fetch(user_data.repos_url)
     .then(response => response.json())
+    .then(repos => repos.forEach(repo => insert_repository(repo)))
+}
+
+function insert_repository(repo){
+    console.log(repo);
+    const new_repo_div = document.createElement("div");
+    new_repo_div.style.borderBottom = "0.5px solid black";
+    const new_name = document.createElement("p");
+    new_name.innerHTML = repo.name;
+    const new_description = document.createElement("p")
+    new_description.innerHTML = repo.description
+    new_repo_div.appendChild(new_name)
+    new_repo_div.appendChild(new_description)
+    document.getElementById("user-repos").appendChild(new_repo_div)
 }
